e7d9f4c83f5a8b9530c85fa931a38b8b66e66380
Wovasya <v37444170895@gmail.com>
2023-04-17 15:40:37 +0400

    Add test

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnPostRequest() {
+
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"));
+    }
+}
